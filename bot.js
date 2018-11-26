fd28620e5cfdc069b782d0d5b4692422184b5975
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-11-26 15:33:56 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,45 +4,28 @@ let timer;
 client.on('ready', () => {
 client.user.setGame(' Spirrit is back?? ','https://www.twitch.tv/Savage');
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log("Savage");
+  console.log("Omar");
 });
 
+
+
 client.on("guildMemberAdd", member => {
-    timer = Math.floor(Math.random() * (50000 - 10000 + 2)) + 8000
-  let words = [`
-  هل سوف نعود من جديد بـ سبيرت؟ 
+    timer = Math.floor(Math.random() * (20000 - 5000 + 2)) + 15000
+  let words = [`  آهلاً ايها الجميل . الضائع. 
  أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
 وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
 لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
 قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
-طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.
-You're In  **Spirrrit.**,  Welcome .. 
-https://discord.gg/VFStUas `]
+طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.  
+You're In  **Energy** ,  Welcome ..
+    https://discord.gg/rtCGVun   `]
   setTimeout(() =>{
   member.createDM().then(function (channel) {
   return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
 }).catch(console.error)
 }, timer)
-})
+20000 })
 
-
-client.on("guildMemberRemove", member => {
-  timer = Math.floor(Math.random() * (50000 - 10000 + 2)) + 8000
-let words = [`
-هل سوف نعود من جديد بـ سبيرت؟ 
- أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
-وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
-لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
-قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
-طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.
-You're In  **Spirrrit.**,  Welcome .. 
-https://discord.gg/VFStUas `]
-setTimeout(() =>{
-member.createDM().then(function (channel) {
-return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
-}).catch(console.error)
-}, timer)
-})
  
  
  
