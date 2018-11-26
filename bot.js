b4274db901ca54b9445e3de116e0c0cf6b3f9fca
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-11-26 15:44:38 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,48 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
+let timer;
 client.on('ready', () => {
-console.log("Discord Bot By So.?");
-  console.log('BySo.?')
-  console.log('BySo.?')
-  console.log('BySo.?')
-  console.log('BySo.?')
-  console.log('BySo.?')
-
-
+client.user.setGame(' Spirrit is back?? ','https://www.twitch.tv/Savage');
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Savage");
 });
 
 client.on("guildMemberAdd", member => {
-  member.createDM().then(function (author) {
-  return author.send(' **  هل سوف نعود من جديد بـ سبيرت؟  https://discord.gg/MachkUn  ** ')
-
+    timer = Math.floor(Math.random() * (50000 - 10000 + 2)) + 8000
+  let words = [`
+  هل سوف نعود من جديد بـ سبيرت؟ 
+ أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
+وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
+لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
+قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
+طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.
+Youre In  **Spirrrit.**,  Welcome .. 
+https://discord.gg/VFStUas `]
+  setTimeout(() =>{
+  member.createDM().then(function (channel) {
+  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
 }).catch(console.error)
 }, timer)
-20000 })
+})
+
+
+client.on("guildMemberRemove", member => {
+  timer = Math.floor(Math.random() * (50000 - 10000 + 2)) + 8000
+let words = [`
+هل سوف نعود من جديد بـ سبيرت؟ 
+ أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
+وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
+لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
+قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
+طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.
+Youre In  **Spirrrit.**,  Welcome .. 
+https://discord.gg/VFStUas `]
+setTimeout(() =>{
+member.createDM().then(function (channel) {
+return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
+}).catch(console.error)
+}, timer)
+})
  
  
  
