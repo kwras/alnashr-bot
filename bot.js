61965d95336eadfdfbf21fea905d84a5b11f1519
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-11-26 15:40:21 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,23 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-let timer;
 client.on('ready', () => {
-client.user.setGame(' Spirrit is back?? ','https://www.twitch.tv/Savage');
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log("Omar");
+console.log("Discord Bot By So.?");
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+
+
 });
 
-
-
 client.on("guildMemberAdd", member => {
-    timer = Math.floor(Math.random() * (20000 - 5000 + 2)) + 15000
-  let words = [`  آهلاً ايها الجميل . الضائع. 
- أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
-وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
-لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
-قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
-طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.  
-*Welcome To* **Spirrrit** .
-    https://discord.gg/rtCGVun   `]
-  setTimeout(() =>{
-  member.createDM().then(function (channel) {
-  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
+  member.createDM().then(function (author) {
+  return author.send(' **  هل سوف نعود من جديد بـ سبيرت؟  https://discord.gg/MachkUn  ** ')
+
 }).catch(console.error)
 }, timer)
 20000 })
-
- 
  
  
  
