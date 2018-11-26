981ee46483ff181f27b42f1cd5c6952e8c90ef18
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-11-26 15:29:53 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,82 +1,48 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "2968"// البرفكس هنا
-
+let timer;
 client.on('ready', () => {
+client.user.setGame(' Spirrit is back?? ','https://www.twitch.tv/Savage');
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('By Wenteed')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-
-  
-  
-
-
-});
- 
- client.on('ready',async () => {
-console.log("Starting..");
-let g = client.guilds.get("499633648866164737");
-let c = g.channels.get("500651445327233034");
-if(c.type === 'voice') {
-c.join();
-setInterval(() => {
-if(!g.me.voiceChannel) c.join();
-}, 1);
-} else {
-console.log("Failed To Join:\n The Channel Type isn't \"text\"");
-}
-});
- 
- 
-const adminprefix = "2968";
-const devs = ['506211849100984320'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-   
-if (message.content.startsWith(adminprefix + 'setgame')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'av')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else    
-if (message.content.startsWith(adminprefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
-}
- 
+  console.log("Savage");
 });
 
+client.on("guildMemberAdd", member => {
+    timer = Math.floor(Math.random() * (50000 - 10000 + 2)) + 8000
+  let words = [`
+  هل سوف نعود من جديد بـ سبيرت؟ 
+ أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
+وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
+لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
+قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
+طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.
+You're In  **Spirrrit.**,  Welcome .. 
+https://discord.gg/VFStUas `]
+  setTimeout(() =>{
+  member.createDM().then(function (channel) {
+  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
+}).catch(console.error)
+}, timer)
+})
 
 
-
-
+client.on("guildMemberRemove", member => {
+  timer = Math.floor(Math.random() * (50000 - 10000 + 2)) + 8000
+let words = [`
+هل سوف نعود من جديد بـ سبيرت؟ 
+ أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
+وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
+لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
+قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
+طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.
+You're In  **Spirrrit.**,  Welcome .. 
+https://discord.gg/VFStUas `]
+setTimeout(() =>{
+member.createDM().then(function (channel) {
+return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
+}).catch(console.error)
+}, timer)
+})
  
  
  
