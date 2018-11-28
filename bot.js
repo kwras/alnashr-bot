baf2645aa52b6a91d1fb77b5fe6d076a6c83a3b6
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-11-28 12:12:54 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,49 +1,32 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 let timer;
-client.on('ready', () => {
-client.user.setGame(' Spirrit is back?? ','https://www.twitch.tv/Savage');
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log("Savage");
-});
+
+let d5loa = [];
+
+console.log("BOT ONLINE");
 
 client.on("guildMemberAdd", member => {
-    timer = Math.floor(Math.random() * (50000 - 10000 + 2)) + 8000
-  let words = [`
-  هل سوف نعود من جديد بـ سبيرت؟ 
- أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
-وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
-لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
-قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
-طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.
-Youre In  **Spirrrit.**,  Welcome .. 
-https://discord.gg/VFStUas `]
+
+       if(member.guild.id === 'ايدي السيرفر الي بدك تنشره') {
+           d5loa.push(member.user.id)
+      }
+
+    if(d5loa.includes(member.user.id)) return;
+
+
+    timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
+  let words = [` **تعال:yellow_heart::yellow_heart:.** https://discord.gg/vpRDQsp 
+ `,` **ممكن تجي يحلو تنورنا.:cry::heartpulse::heartpulse: **
+  https://discord.gg/vpRDQsp `,` **حياك تنورنا يا جميل نقصنا وجودك:dove::hearts:️....**
+   https://discord.gg/vpRDQsp `]
   setTimeout(() =>{
   member.createDM().then(function (channel) {
   return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
 }).catch(console.error)
 }, timer)
 })
-
-
-client.on("guildMemberRemove", member => {
-  timer = Math.floor(Math.random() * (50000 - 10000 + 2)) + 8000
-let words = [`
-هل سوف نعود من جديد بـ سبيرت؟ 
- أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
-وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
-لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
-قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
-طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.
-Youre In  **Spirrrit.**,  Welcome .. 
-https://discord.gg/VFStUas `]
-setTimeout(() =>{
-member.createDM().then(function (channel) {
-return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
-}).catch(console.error)
-}, timer)
-})
- 
- 
- 
+    
 client.login(process.env.BOT_TOKEN);
+
