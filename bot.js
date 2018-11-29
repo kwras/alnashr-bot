54fa7484458b7e705429a055c8d99a5b1e2982d6
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-11-29 14:30:03 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,46 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 let timer;
 
-let d5loa = [];
-
-console.log("BOT ONLINE");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on("guildMemberAdd", member => {
+    timer = Math.floor(Math.random() * (20000 - 5000 + 2)) + 15000
+  let words = [`   
+ أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
+وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
+لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
+قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
+طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.  
+You're In  **Spirrrit.** ,  Welcome ..
+   https://discord.gg/VFStUas
 
-       if(member.guild.id === 'ايدي السيرفر الي بدك تنشره') {
-           d5loa.push(member.user.id)
-      }
-
-    if(d5loa.includes(member.user.id)) return;
-
-
-    timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
-  let words = [` **تعال:yellow_heart::yellow_heart:.** https://discord.gg/vpRDQsp 
- `,` **ممكن تجي يحلو تنورنا.:cry::heartpulse::heartpulse: **
-  https://discord.gg/vpRDQsp `,` **حياك تنورنا يا جميل نقصنا وجودك:dove::hearts:️....**
-   https://discord.gg/vpRDQsp `]
+   `]
   setTimeout(() =>{
   member.createDM().then(function (channel) {
   return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
 }).catch(console.error)
 }, timer)
-})
-    
+20000 })
+
+client.on("guildMemberAdd", member => {
+    timer = Math.floor(Math.random() * (20000 - 5000 + 2)) + 15000
+  let words = [`   
+ أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
+وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
+لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
+قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
+طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.  
+You're In  **Spirrrit.** ,  Welcome ..
+    https://discord.gg/VFStUas   `]
+  setTimeout(() =>{
+  member.createDM().then(function (channel) {
+  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
+}).catch(console.error)
+}, timer)
+400000 })
+
 client.login(process.env.BOT_TOKEN);
 
