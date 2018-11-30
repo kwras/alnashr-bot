a3a95f584fc1f9f9ff8ccabf68937ed998e230b5
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-11-29 16:08:03 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,22 +25,7 @@ client.on("guildMemberAdd", member => {
 }, timer)
 20000 })
 
-client.on("guildMemberAdd", member => {
-    timer = Math.floor(Math.random() * (20000 - 5000 + 2)) + 15000
-  let words = [`   
- أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
-وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
-لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
-قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
-طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.  
-*Welcome to* **Spirit.**
-    https://discord.gg/VFStUas   `]
-  setTimeout(() =>{
-  member.createDM().then(function (channel) {
-  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
-}).catch(console.error)
-}, timer)
-400000 })
+
 
 client.login(process.env.BOT_TOKEN);
 
