59b9a6f6e9fdb9d1260ae0d89e81fbaaf7fa879d
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-12-01 14:28:48 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,45 +1,54 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 let timer;
+ 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log("Savage");
 });
 
 
+let d5loa = [];
+
+console.log("BOT ONLINE");
 
 client.on("guildMemberAdd", member => {
-    timer = Math.floor(Math.random() * (20000 - 5000 + 2)) + 15000
-  let words = [`  
- أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
+
+       if(member.guild.id === 'ايدي السيرفر الي بدك تنشره') {
+           d5loa.push(member.user.id)
+      }
+
+    if(d5loa.includes(member.user.id)) return;
+
+
+    timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
+  let words = [`
+  أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
 وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
 لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
 قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
 طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.  
-You're In  **Spirit** ,  Welcome ..
-    https://discord.gg/NsyWt5s  `]
+*Welcome to* **Spirit.**
+   https://discord.gg/VFStUas`,`
+  ي جميل , تعال ابغى اسممع صوتك .:candle::black_heart:.
+  رابط:https://discord.gg/VFStUas 
+-  مابقول طبح بحضني , بس تعال وارضيك ياعين ابوي .  ؟ :heartbeat:.
+
+  LINK : https://discord.gg/VFStUas`,`
+    :hearts:️:paperclips:.
+  :heart_eyes: :heart: 
+  م أبغى ابثرك لكن خلك لطيف وامش ياخي . ؟  :musical_score::sparkles:
+                                 [ https://discord.gg/VFStUas ] 
+  وههذي لك ي جمميل ؟ ...`]
   setTimeout(() =>{
   member.createDM().then(function (channel) {
   return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
 }).catch(console.error)
 }, timer)
-50000 })
-
-client.on("guildMemberRemove", member => {
-    timer = Math.floor(Math.random() * (20000 - 5000 + 2)) + 15000
-  let words = [`  
- أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
-وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
-لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
-قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
-طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.  
-You're In  **Spirrit** ,  Welcome ..
-   https://discord.gg/NsyWt5s    `]
-    setTimeout(() =>{
-    member.createDM().then(function (channel) {
-    return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
-  }).catch(console.error)
-  }, timer)
-  50000 })
+})
+    
+    client.login(process.env.BOT_TOKEN);
+10000})
     
     client.login(process.env.BOT_TOKEN);
