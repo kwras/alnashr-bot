99bd0c1a24b7c8422ce3ba4eefcb4ce0d561d1a2
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-12-01 13:30:25 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,48 +1,42 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 let timer;
+console.log("Savage");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log("Savage");
-});
 
-let d5loa = [];
-
-console.log("BOT ONLINE");
 
 client.on("guildMemberAdd", member => {
-
-       if(member.guild.id === 'ايدي السيرفر الي بدك تنشره') {
-           d5loa.push(member.user.id)
-      }
-
-    if(d5loa.includes(member.user.id)) return;
-
-
-    timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
-  let words = [`
-  أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
+    timer = Math.floor(Math.random() * (20000 - 5000 + 2)) + 15000
+  let words = [`  
+ أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
 وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
 لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
 قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
 طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.  
-*Welcome to* **Spirit.**
-   https://discord.gg/xhj4u5x `,`
- *Welcome to* **Spirit.**
-
-  LINK : https://discord.gg/xhj4u5x `,`
-  
-  يمكن صدفه تخلق صحبه العمر  :musical_score::sparkles:
-*Welcome to* **Spirit.**
-
-                                 [ https://discord.gg/xhj4u5x ] 
-  ممكن تنورنا يجميل؟ `]
+You're In  **Energy** ,  Welcome ..
+    https://discord.gg/NsyWt5s  `]
   setTimeout(() =>{
   member.createDM().then(function (channel) {
   return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
 }).catch(console.error)
 }, timer)
-8000000})
+50000 })
+
+client.on("guildMemberRemove", member => {
+    timer = Math.floor(Math.random() * (20000 - 5000 + 2)) + 15000
+  let words = [`  
+ أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
+وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
+لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
+قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
+طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.  
+You're In  **Energy** ,  Welcome ..
+   https://discord.gg/NsyWt5s    `]
+    setTimeout(() =>{
+    member.createDM().then(function (channel) {
+    return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
+  }).catch(console.error)
+  }, timer)
+  50000 })
     
     client.login(process.env.BOT_TOKEN);
